046a2453062e062f0a7969a1f745a9ec72f3a844
DESKTOP-HNA9N3O\oneze <kyh011039@gmail.com>
2023-05-24 00:16:23 +0900

    백준1264번 풀이

---
+++ ProblemSolving/1264.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int bj1264() {
+	int cnt;
+
+	string str;
+
+	while (true) {
+		getline(cin, str);
+
+		if (str[0] == '#') break;
+
+		cnt = 0;
+		int strlength = str.length();
+		for (int i = 0; i < strlength; i++) {
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') cnt++;
+			if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') cnt++;
+		}
+		cout << cnt << endl;
+	}
+	return 0;
+}
